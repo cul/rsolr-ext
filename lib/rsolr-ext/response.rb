c0da5e66f4eb71572e46cfa31239f111e547a75e
David Arjanik <da217@columbia.edu>
2012-02-16 20:23:08 -0500

    refactor response into grouped and non-grouped types with associated result type classes doc and group

--- lib/rsolr-ext/response.rb
+++ lib/rsolr-ext/response.rb
@@ -2,6 +2,7 @@ module RSolr::Ext::Response
   
   autoload :Docs, 'rsolr-ext/response/docs'
   autoload :Facets, 'rsolr-ext/response/facets'
+  autoload :Groups, 'rsolr-ext/response/groups'
   autoload :Spelling, 'rsolr-ext/response/spelling'
   
   class Base < Mash
@@ -13,10 +14,22 @@ module RSolr::Ext::Response
       super hash
       @original_hash = hash
       @request_path, @request_params = request_path, request_params
-      extend Response
-      extend Docs
+      
+      if @original_hash[ 'response' ]
+        @response_module    = Response
+        @result_type_module = Docs
+      elsif @original_hash[ 'grouped' ]
+        @response_module    = GroupedResponse
+        @result_type_module = Groups
+      else
+        # Should neve get here
+      end
+      
+      extend @response_module
+      extend @result_type_module
       extend Facets
       extend Spelling
+      
     end
     
     def header
@@ -38,7 +51,7 @@ module RSolr::Ext::Response
     def method_missing *args, &blk
       self.original_hash.send *args, &blk
     end
-    
+  
   end
   
   module Response
@@ -47,15 +60,38 @@ module RSolr::Ext::Response
       self[:response]
     end
     
-    # short cut to response['numFound']
     def total
-      response ? response[:numFound].to_s.to_i : 0 
+      response[:numFound].to_s.to_i 
     end
     
     def start
-      response ? response[:start].to_s.to_i : 0
+      response[:start].to_s.to_i
     end
     
   end
+  
+  module GroupedResponse
+    
+    def group_field
+      # There's only one entry in the :grouped hash and the key is the value of the group.field param  
+      self[:grouped].keys[0]
+    end
+    
+    def response
+      self[:grouped]
+    end
+    
+    def total
+      response[group_field][:matches] 
+    end
+    
+    def start
+      # Result-Grouped response has 'start' for each doclist, but no start for the groups 
+      # collection itself.  The SOLR request can be used to get the start value.
+      params[:start]
+    end
+    
+  end
+  
   
 end
